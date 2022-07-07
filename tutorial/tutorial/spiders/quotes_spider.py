e338141412073779ae4d11b9f31c75210ccd113c
akhisreelibra <akhisreelibra@gmail.com>
2022-07-07 09:55:50 +0530

    one india

--- tutorial/tutorial/spiders/quotes_spider.py
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -1,63 +1,54 @@
 import scrapy
 import json
+from datetime import timedelta, datetime
+
 
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
 
     def start_requests(self):
-        urls = [
-            # 'https://www.manoramaonline.com/content/mm/mo/news/just-in/_jcr_content/mm-subsection-top-left/articlelisting.results.0.2500.json',
-            # 'https://www.manoramaonline.com/content/mm/mo/news/just-in/_jcr_content/mm-subsection-top-left/articlelisting.results.2500.2500.json',
-            # 'https://www.manoramaonline.com/content/mm/mo/news/just-in/_jcr_content/mm-subsection-top-left/articlelisting.results.5000.2500.json',
-            # 'https://www.manoramaonline.com/content/mm/mo/news/just-in/_jcr_content/mm-subsection-top-left/articlelisting.results.7500.2500.json',
-            # 'https://www.manoramaonline.com/content/mm/mo/news/just-in/_jcr_content/mm-subsection-top-left/articlelisting.results.10000.2500.json',
-            # 'https://www.manoramaonline.com/content/mm/mo/news/just-in/_jcr_content/mm-subsection-top-left/articlelisting.results.12500.2500.json',
-            # 'https://www.manoramaonline.com/content/mm/mo/news/just-in/_jcr_content/mm-subsection-top-left/articlelisting.results.15000.2500.json',
-            # 'https://www.manoramaonline.com/content/mm/mo/news/just-in/_jcr_content/mm-subsection-top-left/articlelisting.results.17500.2500.json',
-            # 'https://www.manoramaonline.com/content/mm/mo/news/just-in/_jcr_content/mm-subsection-top-left/articlelisting.results.20000.2500.json',
-            # 'https://www.manoramaonline.com/content/mm/mo/news/just-in/_jcr_content/mm-subsection-top-left/articlelisting.results.22500.2500.json',
-            # 'https://www.manoramaonline.com/content/mm/mo/news/just-in/_jcr_content/mm-subsection-top-left/articlelisting.results.25000.2500.json',
-            # 'https://www.manoramaonline.com/content/mm/mo/news/just-in/_jcr_content/mm-subsection-top-left/articlelisting.results.27500.2500.json',
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.write_url_to_file)
+        urls = []
+        global base_url
+        base_url = 'https://malayalam.oneindia.com'
+        date = datetime.strptime('2020/05/01', '%Y/%m/%d').date()
+        # print(date, type(date), date.strftime('%d/%m/%Y'), type(str(date)))
 
+        while date != datetime.strptime('2022/04/20', '%Y/%m/%d').date():
+            date += timedelta(days=1)
+            url = base_url + "/" + str(date.strftime('%Y/%m/%d')) + "/"
+            urls.append(url)
+        for url in urls:
+            try:
+                yield scrapy.Request(url=url, callback=self.write_url_to_file)
+            except:
+                pass
     def write_url_to_file(self, response):
-        fullnewslist = f'fullnewslist.txt'
-        with open(fullnewslist, 'wb') as f:
-            f.write(response.body)
-        return self.get_single_news_body()
-    
-    def get_single_news_body(self):
-        fullnewslist = f'fullnewslist.txt'
-        with open(fullnewslist, 'r', encoding="utf-8") as f:
-            f = f.read()
-            f = f.replace("'", '"')
-            jj = json.loads(f)
-            urls_list =[]
-            for i in json.loads(jj['articleList']):
-                urls_list.append(i['url'])
-            for url in urls_list:
+        for story in response.css('div.dayIndexContent'):
+            headline = story.css('a::attr(href)').get(),
+            try:
+                url = base_url+headline[0]
                 yield scrapy.Request(url=url, callback=self.save_fetched_json)
+            except:
+                pass
+
+    def get_single_news_body(self, response):
+        story = ""
+        for headline in response.css('h1.heading::text'):
+            headline = headline.get()
+        for sent in response.css('div.oi-article-lt p::text')[:2]:
+            story = story + " " + sent.get()
+        yield {
+            'headline': headline,
+            'para': story
+        }
+
     def save_fetched_json(self, response):
 
-        resp = self.parse_html_from_url(response)
-        c = 0        
-        
+        resp = self.get_single_news_body(response)
+        c = 0
+
         for i in resp:
             with open('article.json', 'a', encoding='utf-8') as f:
                 c = c+1
                 # print("Dumped ", c, " docs")
-                json.dump(i, f, ensure_ascii=False, indent=4)
-        
-    def parse_html_from_url(self, response):
-        headline = ""
-        para = ""
-        for story in response.css('div.story-header'):
-            headline = story.css('h1.story-headline::text').get(),
-        for article in response.css('div.rte-article'):
-            para = article.css('p::text').get(),
-        yield {
-            'headline' : headline[0],
-            'para': para[0]
-            }
+                json.dump(i, f, ensure_ascii=False, indent=None)
